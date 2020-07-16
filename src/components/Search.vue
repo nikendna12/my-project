726ea3f1d84025ed0d18cdc72e6f88ac4a3a81b1
Desi Niken Ayu <desinikenayu2@gmail.com>
2020-07-16 16:40:13 +0700

    change API and add bootstrap

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class='search'>
-    <h1>Searchh</h1>
-    <input type='text' v-model='query' @keyup='getResult'>
-    <div v-for='result in results' :key='result.id'>
-      <p>{{ result.title }}</p>
-      <img v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" width='100px'>
+  <div class="container-fluid">
+    <div class="page-header">
+    	<h1>Movies</h1>
+    </div>
+    <div>
+      <div v-for='result in results' :key='result.id' class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+        <div class="thumbnail">
+          <p>{{ result.title }}</p>
+          <img v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" width='100px' class="poster">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,12 +25,22 @@ export default {
   },
   methods: {
     getResult() {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=2842841078a4f6bfc31aefd8209023d7&query=' + this.query)
+      axios.get('https://api.themoviedb.org/3/movie/popular', {
+        params: {
+          api_key: '2842841078a4f6bfc31aefd8209023d7',
+          language: 'en-US',
+          page: 2,
+          region:'ID'
+        }
+      })
         .then(response => {
           this.results = response.data.results 
         })
       console.log(this.results)
     }
+  },
+  mounted() {
+    this.getResult()
   }
 }
 </script>
